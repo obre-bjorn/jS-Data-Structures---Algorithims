790bfaf7598c182ff5d2d38cae57a2d3ec44e3fc
obre.bjorn <obre.bjorn@gmail.com>
2022-05-19 16:04:47 +0300

    gcd of 2 numbers

---
+++ Algorithms/Recursion/gcd.js
@@ -0,0 +1,12 @@
+function gcd(num1, num2) {
+    // Base Class
+    if (!num2) {
+        return num1
+    } else {
+        //recursive case
+        return gcd(num2, num1 % num2)
+    }
+
+}
+
+console.log(gcd(15, 12))
